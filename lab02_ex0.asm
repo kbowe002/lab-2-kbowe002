ea1aa7f2174b4e8d9b70fb810a72034a199750c8
Kyle Bowen <kbowe002@ucr.edu>
2017-01-19 14:08:18 -0800

    lab2 ex0 first draft

--- lab02_ex0.asm
+++ lab02_ex0.asm
@@ -1,9 +1,23 @@
 ;=================================================
-; Name: 
-; Email:  
+; Name: Kyle Bowen
+; Email: kbowe002@ucr.edu
 ; 
 ; Lab: lab 2
-; Lab section: 
-; TA: 
+; Lab section: 021
+; TA: Kenneth
 ; 
 ;=================================================
+; Hello World example program
+; Also illustrates how to use PUTS (aka Trap x22)
+;
+     .orig x3000
+  ; Instructions
+  LEA R0, MSG_TO_PRINT
+  PUTS
+
+  HALT
+  ; Local Data
+  MSG_TO_PRINT	.STRINGZ	"Hello world!!!/n"
+
+
+.END
